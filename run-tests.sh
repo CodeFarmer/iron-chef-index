2eea6e8916cb810d66d7621b5a54d4614d392f48
Joel Gluth <joel@gluth.io>
2023-04-13 18:56:45 +0100

    Wrap unit tests in database transactions to isolate them (and get rid of the index-tests database)

--- run-tests.sh
+++ run-tests.sh
@@ -1,5 +1,4 @@
 #! /usr/bin/env bash
 
 sqlite3 unit-tests.sqlite < index.sql
-sqlite3 index-tests.sqlite < index.sql
 lein test
--- test/iron_chef_index/core_test.clj
+++ test/iron_chef_index/core_test.clj
@@ -4,10 +4,9 @@
             [iron-chef-index.core :refer :all]))
 
 (def ds (jdbc/get-datasource {:dbtype "sqlite" :dbname "unit-tests.sqlite"}))
-(def ex-ds (jdbc/get-datasource {:dbtype "sqlite" :dbname "index-tests.sqlite"}))
 
 (deftest chef-test
-  (with-open [conx (jdbc/get-connection ds)]
+  (jdbc/with-transaction [conx ds]
     (testing "start out with 0 chefs"
       (is (empty? (get-all-chefs conx))))
     (testing "chef creation"
@@ -28,7 +27,8 @@
 
     (testing "name fixup works"
       (is (= (get-or-create-chef-id! conx "Kōji Kobayashi" "French")
-             (:chefs/id (get-chef-by-name conx "Koji Kobayashi")))))))
+             (:chefs/id (get-chef-by-name conx "Koji Kobayashi")))))
+    (.rollback conx)))
 
 
 (deftest element-text-test
@@ -131,7 +131,7 @@
       (is (= "France" (challenger-nationality jacques-row))))))
 
 (deftest execute-test
-  (with-open [conx (jdbc/get-connection ex-ds)]
+  (jdbc/with-transaction [conx ds]
     (execute! conx)
     (testing "After execution, the correct number of episodes is created"
       (is (= 59 (count (get-all-episodes conx)))))
@@ -139,4 +139,5 @@
       (is (= 63 (count (get-all-chefs conx)))))
     ;; TODO this should be derived from clj results not sql
     (testing "The right number of iron chefs should be allocated during the series"
-      (is (= 4 (second (first (jdbc/execute-one! conx ["select count(distinct(iron_chef_id)) from iron_chefs_episodes"]))))))))
+      (is (= 4 (second (first (jdbc/execute-one! conx ["select count(distinct(iron_chef_id)) from iron_chefs_episodes"]))))))
+    (.rollback conx)))
